f9f3cc77aef8bd047eb8882e42754ed879517554
Riccardo Ragazzo <ragazzoricco@gmail.com>
2025-01-15 18:22:55 +0000

    Add points tag in  index.html, correct if statement

--- gameMain.js
+++ gameMain.js
@@ -1,16 +1,54 @@
 // Class Game Logic
 class Game {
   constructor() {
-  this.gameEngine
+    this.gameEngine;
+    this.inGameGraphic;
   }
 
-  gameEngine(humanChoice, computerChoice){
+  gameEngine(humanChoice, computerChoice) {
+    let winner = "";
+    this.points = 0;
 
     // Cases for the choices
-    if(humanChoice === computerChoice){
-      console.log('Draw, do it again');
+    if (humanChoice === computerChoice) {
+      console.log("Draw, do it again");
+      winner = "Draw";
+    } else if (humanChoice === "rock") {
+      winner = computerChoice === "paper" ? "Computer" : "Human";
+    } else if (humanChoice === "paper") {
+      winner = computerChoice === "scissors" ? "Computer" : "Human";
+    } else if (humanChoice === "scissors") {
+      winner = computerChoice === "paper" ? "Human" : "Computer";
     }
 
+    if (winner === "Human") {
+      this.points += 3;
+    } else if (winner === "Computer" && this.points > 0) {
+      this.points -= 1;
+    } 
+    console.log("Winner is: ", winner);
+    this.inGameGraphic(humanChoice, computerChoice, winner);
+  }
+
+  inGameGraphic(humanChoice, computerChoice, winner) {
+    const humanChoiceText = document.getElementById("humanChoice");
+    const computerChoiceText = document.getElementById("computerChoice");
+    const winnerText = document.getElementById("winner");
+    const pointsText = document.getElementById('points')
+    humanChoiceText.innerHTML = humanChoice;
+    computerChoiceText.innerHTML = computerChoice;
+    pointsText.innerHTML = `${this.points}`
+
+    if (winner === "Human") {
+      winner = '<h1 style="color: green;">Human</h1>';
+    } else if (winner === "Computer") {
+      winner = '<h1 style="color: red;">Computer</h1>';
+    } else {
+      winner = '<h1 style="color: yellow;">Draw</h1>';
+    }
+
+    console.log(winner);
+    winnerText.innerHTML = winner;
   }
 }
 
@@ -18,15 +56,14 @@ class Game {
 class Player {
   constructor(userName) {
     this.userName = userName;
-    const game = new Game()
-    this.game = game
+    const game = new Game();
+    this.game = game;
   }
 
   chooseRoPaSC(humanChoice, computerChoice) {
-
-    console.log("Button is clicked for human value: ",humanChoice);
-    console.log("Button is clicked for Computer value: ",computerChoice);
-    this.game.gameEngine(humanChoice, computerChoice)
+    console.log("Button is clicked for human value: ", humanChoice);
+    console.log("Button is clicked for Computer value: ", computerChoice);
+    this.game.gameEngine(humanChoice, computerChoice);
   }
 }
 
@@ -35,10 +72,9 @@ class ComputerPlayer extends Player {
   constructor(userName = "Computer") {
     super(userName);
     const newPlayer = new Player(); // Instantiate a new Player class object
-    this.newPlayer = newPlayer
+    this.newPlayer = newPlayer;
     this.randomChoice; // Instantiate a new function for random choice
   }
-
 
   randomChoice = () => {
     let choice;
@@ -53,8 +89,8 @@ class ComputerPlayer extends Player {
     console.log("Random number ", randomNumber);
     console.log("Choice:  ", choice);
     //this.newPlayer.chooseRoPaSC(choice); // Invoke this. to force to recognize the Player object
-    return choice
-  }
+    return choice;
+  };
 }
 
 // Class Human Player
@@ -62,7 +98,7 @@ class HumanPlayer extends Player {
   constructor(userName = "Human") {
     super(userName);
     const newPlayer = new Player(); // Instantiate a new Player class object
-    const newComputerPlayer = new ComputerPlayer()
+    const newComputerPlayer = new ComputerPlayer();
     const buttons = document.querySelectorAll(".buttonChoice"); // Select all the buttons of this class
     buttons.forEach((button) => {
       button.addEventListener("click", (eventValue) => {
@@ -70,7 +106,6 @@ class HumanPlayer extends Player {
         const choice = eventValue.target.value; // Retrieve the value of the button (event) clicked
         //console.log("You clicked: ", buttonValue);
         newPlayer.chooseRoPaSC(choice, newComputerPlayer.randomChoice()); // Call the chooseRoPaSC in the Parent Class
-       
       });
     });
 
